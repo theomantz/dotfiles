3545f98499a7672c6440c308c1b7e6346cee734f
theomantz <theo@mantz.nyc>
2023-10-31 21:04:08 -0400

    update

--- nix/home.nix
+++ nix/home.nix
@@ -3,9 +3,8 @@ let
   tex = (pkgs.texlive.combine {
     inherit (pkgs.texlive) scheme-medium
       dvisvgm dvipng chktex xetex luatex # for preview and export as html
-      wrapfig amsmath ulem hyperref capt-of 
-	  fullpage;
-      (setq org-latex-compiler "lualatex")
+      wrapfig amsmath ulem hyperref capt-of fullpage;
+      #(setq org-latex-compiler "lualatex")
       #(setq org-preview-latex-default-process 'dvisvgm)
   });
 in

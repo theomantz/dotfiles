ffae24202720efcb9efb9120b699ce39eeabbaae
theomantz <theo@mantz.nyc>
2023-11-01 15:55:35 -0400

    update latex packages

--- nix/home.nix
+++ nix/home.nix
@@ -3,7 +3,7 @@ let
   tex = (pkgs.texlive.combine {
     inherit (pkgs.texlive) scheme-medium
       dvisvgm dvipng chktex xetex luatex # for preview and export as html
-      wrapfig amsmath ulem hyperref capt-of;
+      wrapfig amsmath ulem hyperref capt-of geometry;
       #(setq org-latex-compiler "lualatex")
       #(setq org-preview-latex-default-process 'dvisvgm)
   });

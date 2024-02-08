f78c1de2fc4a888e210d213b1532d07e445f93e2
theomantz <theo@mantz.nyc>
2024-02-08 11:56:27 -0500

    remove whatsapp, google chrome casks.

--- nix/casks.nix
+++ nix/casks.nix
@@ -2,16 +2,15 @@
 
 [
 	"signal"
-	"zoom"
 	"firefox-developer-edition"
 	"opera"
 	"iterm2"
 	"bitwarden"
 	"steam"
 	"logi-options-plus"
-	"google-chrome"
 	"google-chrome-dev"
-	"whatsapp"
 	"warp"
   "arc"
+  "amethyst"
+  "goland"
 ]
